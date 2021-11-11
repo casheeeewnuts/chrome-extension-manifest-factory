71ed6ddab1ae4c5245efbc2c6f82d5074838df79
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-11-11 18:21:47 +0900

    add main script

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import * as fs from "fs/promises"
+import * as fs from "fs"
+import * as vm from "vm"
+import * as ts from "typescript"
+import {Manifest} from "./manifest";
 
-export async function _export(input: string, output: string) {
-    const json = await import(input)
-    console.log(json)
-    await fs.writeFile(output, JSON.stringify(json, null, 2))
+
+const VM = vm.createContext({
+    exports: module.exports
+})
+
+export function read(path: string): Manifest.Json {
+    const sourceFileContent = fs.readFileSync(path).toString()
+    const transpiledSourceFile = ts.transpileModule(sourceFileContent, {
+        compilerOptions: {
+            module: ts.ModuleKind.CommonJS
+        }
+    })
+
+    const manifestObjectOrUnknown = vm.runInContext(transpiledSourceFile.outputText, VM) as unknown
+
+    if (!isManifestObject(manifestObjectOrUnknown)) {
+        throw new Error("read file could not transpile manifest.json")
+    }
+
+    return manifestObjectOrUnknown
 }
 
-_export(`${__dirname}/json.ts`, "./manifest.json")
+export function write(output: string, manifest: Manifest.Json): void {
+    fs.writeFileSync(output, JSON.stringify(manifest, null, 2))
+}
+
+function isManifestObject(obj: any): obj is Manifest.Json {
+    return obj.manifest_version != null && obj.name != null && obj.version != null
+}
