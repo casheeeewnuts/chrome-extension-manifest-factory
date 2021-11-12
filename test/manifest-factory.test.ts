731f41cbaf69b7c6f39fa665f65434fbae2b5b48
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2021-11-12 12:05:45 +0900

    add test

---
+++ test/manifest-factory.test.ts
@@ -0,0 +1,14 @@
+import * as ManifestFactory from "../src"
+
+describe("ManifestFactory", () => {
+    describe("read()", () => {
+        it("should return object as Manifest.Json", () => {
+            const manifest = ManifestFactory.read(`${__dirname}/manifest.json.ts`)
+
+            expect(manifest.manifest_version).toBeDefined()
+            expect(manifest.name).toBeDefined()
+            expect(manifest.version).toBeDefined()
+            expect(manifest).toMatchSnapshot(`${__dirname}/manifest.json.snapshot`)
+        })
+    })
+})
---
+++ test/manifest.json.ts
@@ -0,0 +1,9 @@
+import {Manifest} from "../src/index"
+
+const json: Manifest.Json = {
+    manifest_version: 3,
+    name: "",
+    version: "1.0.0"
+}
+
+export default json
